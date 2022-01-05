d2afb586b192e6189ef7fd77d31b1b1f06474e5c
ankushdharkar <ankushdharkar@gmail.com>
2022-01-04 16:01:31 -0800

    Resolves #17: Adds https into the dev server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Goals | Real Dev Squad',
+    title: 'Goals',
     htmlAttrs: {
       lang: 'en',
     },
