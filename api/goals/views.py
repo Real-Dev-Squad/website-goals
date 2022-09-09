b08fffdc562808df75fd7357a795e215702ed25d
pallabez <pallabsonowal999@gmail.com>
2022-09-09 12:45:28 +0530

    setup goals backend

--- api/goals/views.py
+++ api/goals/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from goals.models import Goal, UserGoal
+from goals.serializers import GoalSerializer, UserGoalSerialier
+from rest_framework import viewsets
 
-# Create your views here.
+class GoalViewSet(viewsets.ModelViewSet):
+  queryset = Goal.objects.all()
+  serializer_class = GoalSerializer
+
+class UserGoalViewSet(viewsets.ModelViewSet):
+  queryset = UserGoal.objects.all()
+  serializer_class = UserGoalSerialier
