086f0fc4f3dbbc89a125b01f20762e47d7e46ef5
Prakash Choudhary <34452139+prakashchoudhary07@users.noreply.github.com>
2022-09-29 00:52:10 +0530

    Backend/congif setup (#60)
    * feature/added-getting env from config file
    
    * chor: added default.env and gitignored .env
    
    * chore: update requirement.txt
    
    * chore: added new line
    
    * chore: move comment
    
    * chore: add secret_key in sample-local.py
    
    * chore: add secret_key in development.py

--- api/manage.py
+++ api/manage.py
@@ -1,12 +1,43 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+from django.conf import settings
+from django.core.management.commands.runserver import Command as runserver
 import os
 import sys
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DJANGO_ENV=(str, 'DEVELOPMENT')
+)
+
+# Defining constants
+DJANGO_ENV = 'DJANGO_ENV'
+PRODUCTION = 'PRODUCTION'
+TESTING = 'TESTING'
+SETTINGS_PRODUCTION = 'django_jsonapi.settings.production'
+SETTINGS_DEVELOPMENT = 'django_jsonapi.settings.development'
+SETTINGS_TESTING = 'django_jsonapi.settings.testing'
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_jsonapi.settings')
+
+    django_env = env(DJANGO_ENV).upper()
+
+    if (django_env == PRODUCTION):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                              SETTINGS_PRODUCTION)
+    elif (django_env == TESTING):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                              SETTINGS_TESTING)
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                              SETTINGS_DEVELOPMENT)
+
+    # Changing DJANGO PORT
+    runserver.default_port = settings.__getattr__('PORT')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
