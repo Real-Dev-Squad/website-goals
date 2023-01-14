58811854bb7b43f2d7cb2f14bcd60c62846c7daa
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2023-01-14 12:32:50 +0530

    test written for goals app (#86)

---
+++ api/goals/fixtures.py
@@ -0,0 +1,86 @@
+goals = [
+    {
+        "model": "goals.goal",
+        "pk": 1,
+        "fields": {
+            "goalType": "Frontend",
+            "title": "Create TODO APP",
+            "description": "create a todo app in react",
+            "createdBy": "superuser",
+            "createdAt": "2022-12-20T07:45:33.507Z"
+        }
+    },
+    {
+        "model": "goals.goal",
+        "pk": 2,
+        "fields": {
+            "goalType": "Backend",
+            "title": "create TODO API",
+            "description": "create a todo API using express",
+            "createdBy": "superuser",
+            "createdAt": "2022-12-20T07:45:33.507Z"
+        }
+    },
+    {
+        "model": "goals.goal",
+        "pk": 3,
+        "fields": {
+            "goalType": "Design",
+            "title": "Simple Landing Page",
+            "description": "create simple landing page using figma",
+            "createdBy": "superuser",
+            "createdAt": "2022-12-20T07:45:33.507Z"
+        }
+    },
+]
+
+usergoals = [
+    {
+        "pk": 1,
+        "fields": {
+            "userId": "ivinayakg",
+            "percentageCompleted": 0,
+            "assignedBy": "superuser",
+            "status": "ASSIGNED",
+            "goal": 1,
+            "startsOn": "2022-12-20T07:52:51Z",
+            "endsOn": "2022-12-22T07:52:53Z",
+        }
+    },
+    {
+        "pk": 2,
+        "fields": {
+            "userId": "ivinayakg",
+            "percentageCompleted": 0,
+            "assignedBy": "superuser",
+            "status": "ASSIGNED",
+            "goal": 2,
+            "startsOn": "2022-12-20T07:52:51Z",
+            "endsOn": "2022-12-22T07:52:53Z",
+        }
+    },
+    {
+        "pk": 3,
+        "fields": {
+            "userId": "pallab",
+            "percentageCompleted": 0,
+            "assignedBy": "superuser",
+            "status": "ASSIGNED",
+            "goal": 3,
+            "startsOn": "2022-12-20T07:52:51Z",
+            "endsOn": "2022-12-22T07:52:53Z",
+        }
+    },
+    {
+        "pk": 4,
+        "fields": {
+            "userId": "pallab",
+            "percentageCompleted": 0,
+            "assignedBy": "superuser",
+            "status": "ASSIGNED",
+            "goal": 1,
+            "startsOn": "2022-12-20T07:52:51Z",
+            "endsOn": "2022-12-22T07:52:53Z",
+        }
+    }
+]
