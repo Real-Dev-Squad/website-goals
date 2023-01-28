e5c7de6199e9ebc7276efc732f0dd7d32aa0826b
pallabez <pallabsonowal999@gmail.com>
2023-01-29 00:38:17 +0530

    add jsonapi-vuex

--- website/nuxt.config.js
+++ website/nuxt.config.js
@@ -22,6 +22,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/jsonapi-vuex.js', mode: 'client' },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -55,5 +56,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: ['jsonapi-vuex']
   }
 }
