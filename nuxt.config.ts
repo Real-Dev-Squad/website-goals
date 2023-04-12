a85188deaa0b9b1ee021f92c2289c9e50d000e36
pallabez <pallabsonowal999@gmail.com>
2023-04-12 20:33:11 +0530

    fix vuetify setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
   typescript: {
-    strict: true,
-    typeCheck: true,
+    shim: false,
   },
-  css: ['vuetify/lib/styles/main.sass'],
+  css: ['vuetify/styles'],
   build: {
-    transpile: ['vuetify'],
+    transpile: ["vuetify"]
   },
   modules: [
+    'nuxt-icon',
     '@pinia/nuxt',
-    '@pinia-orm/nuxt'
+    '@pinia-orm/nuxt',
+    async (options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', config => { config.plugins?.push(vuetify()) })
+    }
   ],
+  vite: {
+    ssr: {
+      noExternal: ['vuetify'],
+    }
+  },
   pinia: {
     autoImports: [
       // automatically imports `defineStore`
