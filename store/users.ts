36c395997add202baa60de3798ba8c202a06dfbe
pallabez <pallabsonowal999@gmail.com>
2023-04-15 22:03:39 +0530

    add user store

---
+++ store/users.ts
@@ -0,0 +1,31 @@
+import { defineStore } from "pinia";
+import * as userAdapter from "~/adapters/user";
+import { userRepo } from "~/models/User";
+
+export const useUsersStore = defineStore({
+  id: 'user-store',
+  state: () => ({
+    isLoading: false,
+    isValid: false,
+  }),
+  actions: {
+    async fetchUsers() {
+      if (this.isValid) return;
+
+      this.isLoading = true;
+      const users = await userAdapter.fetchUsers();
+
+      userRepo.save(users);
+
+      this.isValid = true;
+      this.isLoading = false;
+    }
+  },
+  getters: {
+    getUserById: () => {
+      return (userId: string) => {
+        return userRepo.find(userId)
+      }
+    }
+  },
+})
