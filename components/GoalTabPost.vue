170360b9e15c925ca361e66b8ee021c9fd0889aa
pallabez <pallabsonowal999@gmail.com>
2023-05-04 15:25:52 +0530

    add goalTabPost toggle button and ux improvement

---
+++ components/GoalTabPost.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-form @submit.prevent="handleSubmit">
+    <v-row>
+      <v-col cols="12" md="12">
+        <v-text-field v-model="form.title" label="Title" @keydown.enter.prevent="handleSubmit" autofocus @blur="$emit('onGoalTabPostBlur')"/>
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import { useGoalsStore } from '~/store/goals'
+
+const goalStore = useGoalsStore()
+const emit = defineEmits(['onGoalTabPostBlur'])
+const form = reactive({
+  title: ''
+})
+
+const handleSubmit = () => {
+  if (!form.title) return
+
+  goalStore.add({
+    title: form.title
+  })
+  form.title = ''
+  emit('onGoalTabPostBlur')
+}
+
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,19 @@
 <template>
   <v-container>
-    <GoalTabAdd :goal="DEFAULT_GOAL" @goal-added="addGoal" />
-    <v-btn @click="showGoalForm = true" color="primary">Show Goal Modal</v-btn>
-    <GoalFormModal :visible="showGoalForm" @close="showGoalForm = false" />
+    <GoalTabPost v-if="showGoalTabPost" @on-goal-tab-post-blur="showGoalTabPost = false"/>
+    <GoalTabPostToggleButton v-else @on-add-goal-button-click="showGoalTabPost = true"/>
   </v-container>
 </template>
 
 <script>
-import GoalFormModal from '~/components/GoalFormModal.vue'
-import GoalTabAdd from '~/components/GoalTabAdd.vue'
-import { DEFAULT_GOAL } from '~/constants/goal'
-
 export default {
   name: 'HomePage',
   data () {
     return {
-      DEFAULT_GOAL,
-      showGoalForm: false
+      showGoalForm: false,
+      showGoalTabPost: false
     }
   },
-  methods: {
-    addGoal (goal) {
-    }
-  },
-  components: { GoalTabAdd, GoalFormModal }
 }
 </script>
 
