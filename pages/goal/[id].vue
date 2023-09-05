4c9c041a61f7ba1927e64242f129f2ddb9e9458d
Pallab Sonowal <pallabsonowal999@gmail.com>
2023-09-05 18:18:11 +0530

    linked url with goal modal

---
+++ pages/goal/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <GoalFormModal
+    :visible="showCreateGoalModal"
+    :close-goal-modal="closeGoalModal"
+  />
+</template>
+
+<script setup lang="ts">
+const showCreateGoalModal = ref(true);
+const router = useRouter()
+const closeGoalModal = () => {
+  router.push('/')
+};
+
+</script>
