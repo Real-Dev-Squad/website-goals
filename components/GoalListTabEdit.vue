7bfb0cd22babfc22bd9865230c88ecc213fef8a7
Pallab Sonowal <pallabsonowal999@gmail.com>
2023-11-10 01:46:32 +0530

    add status menu option in tab (#165)
    * add status menu option in tab
    
    * remove unnecessary change

--- components/GoalListTabEdit.vue
+++ components/GoalListTabEdit.vue
@@ -1,11 +1,10 @@
 <template>
   <tr v-if="goal">
     <td class="column__status">
-      <v-tooltip :text="goalStatus.text" location="left">
-        <template v-slot:activator="{ props }">
-          <v-btn size="10" v-bind="props" :color="goalStatus.color"></v-btn>
-        </template>
-      </v-tooltip>
+      <GoalTabStatusMenu 
+        :goalStatus="goal.status"
+        @status-change="handleStatusChange"
+      />
     </td>
 
     <td class="column__title">
@@ -30,27 +29,21 @@
 <script lang="ts" setup>
 import { goalRepo } from "~/models/Goal";
 import { useGoalsStore } from "~/store/goals";
-import { GOAL_STATUS } from "~/constants/goal";
 
 const props = defineProps(["goalId"]);
 const goalStore = useGoalsStore();
 const goal = computed(() => goalRepo.find(props.goalId));
-const goalStatus = computed(() => {
-  switch (goal.value?.status) {
-    case 'ongoing':
-      return GOAL_STATUS.ONGOING;
-    case 'completed':
-      return GOAL_STATUS.COMPLETED;
-    default:
-      throw Error('Goal status mismatch');
-  }
-})
 
 if (!goal.value) throw new Error("Goal not found");
 
 function handleTitleChange(title: string) {
   goalStore.patch(props.goalId, { title });
 }
+
+function handleStatusChange(status: string) {
+    goalStore.patch(props.goalId, { status })
+}
+
 
 function handleSelectAssignee(selectedAssigneeId: string) {
   goalStore.patch(props.goalId, {
