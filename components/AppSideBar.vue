b87b850f3f3b2511c0e868479a7eed6e5484c2bd
pallabez <pallabsonowal999@gmail.com>
2023-11-26 15:08:56 +0530

    add side bar mobile responsivity

--- components/AppSideBar.vue
+++ components/AppSideBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-navigation-drawer permanent>
+  <v-navigation-drawer permanent :rail="mobile" expand-on-hover>
     <v-list>
       <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/john.png" title="John Leider"
         subtitle="john@google.com">
@@ -9,34 +9,33 @@
     <v-divider></v-divider>
 
     <v-list :lines="false" nav class="d-flex flex-column">
-      <v-list-item v-for="(item, i) in items" :key="i" :value="item" color="primary">
-        <template v-slot:prepend>
-          <v-icon :icon="item.icon"></v-icon>
-        </template>
-
-        <v-list-item-title v-text="item.text"></v-list-item-title>
+      <v-list-item v-for="(item, i) in items" :key="i" :value="item" color="primary" :prepend-icon="item.icon"
+        :title="item.text">
       </v-list-item>
       <v-divider></v-divider>
 
-      <v-list-item class="align-self-center">
-        <v-btn color="primary" prepend-icon="mdi-plus">
-          Create Goal
-        </v-btn>
+      <v-list-item title="Create Goal" prepend-icon="mdi-plus" link class="create-goal">
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
 
-<script lang="ts">
-export default {
-  data: () => ({
-    items: [
-      { text: 'Home', icon: 'mdi-home' },
-      { text: 'All', icon: 'mdi-folder' },
-      { text: 'Mine', icon: 'mdi-account' },
-      { text: 'Marked', icon: 'mdi-star' },
-      { text: 'Activity', icon: 'mdi-history' },
-    ],
-  }),
-}
+<script lang="ts" setup>
+import { useDisplay } from 'vuetify/lib/framework.mjs';
+
+const { mobile } = useDisplay();
+
+const items = [
+  { text: 'Home', icon: 'mdi-home' },
+  { text: 'All', icon: 'mdi-folder' },
+  { text: 'Mine', icon: 'mdi-account' },
+  { text: 'Marked', icon: 'mdi-star' },
+  { text: 'Activity', icon: 'mdi-history' },
+]
 </script>
+
+<style scoped>
+.create-goal {
+  color: var(--primary-color);
+}
+</style>
